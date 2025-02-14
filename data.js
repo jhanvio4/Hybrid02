2da93e8255d0a02af772dac193b92cb64e6aeb25
jhanvio4 <145598331+jhanvio4@users.noreply.github.com>
2025-02-14 16:49:12 -0500

    Complete the assignment

---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Maldives Island",
+        location: "Maldives",
+        googleMapsUrl: "https://www.google.ca/maps/place/Maldives/@3.0988179,67.9401141,6z/data=!3m1!4b1!4m6!3m5!1s0x24b599bfaafb7bbd:0x414509e181956289!8m2!3d3.202778!4d73.22068!16zL20vMDR0eTg?entry=ttu&g_ep=EgoyMDI1MDIxMS4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "12 Nov 2022",
+        endDate: "28 Nov 2022",
+        description: "The Maldives has been an Islamic nation since the 12th century, but was previously Buddhist and Hindu. As an important trading post on the ancient Spice Route between East Asia and the Middle East, there are influences from China, Malaysia, Indonesia, Africa and the Arab world in Maldivian culture.",
+        imageUrl: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/180000/180678-Maldives-All.jpg"
+    },
+    {
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://www.google.fr/maps/place/Paris,+France/@48.8588255,2.2646345,12z/data=!3m1!4b1!4m6!3m5!1s0x47e66e1f06e2b70f:0x40b82c3688c9460!8m2!3d48.8575475!4d2.3513765!16zL20vMDVxdGo?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "15 July 2019",
+        endDate: "05 August 2019",
+        description: "The City of Light draws millions of visitors every year with its unforgettable ambiance. Of course, the divine cuisine and vast art collections deserve some of the credit as well. The gentle River Seine rambles through the city, flanked by stately museums, centuries-old churches, and blocks of Rococo- and Neoclassic-design architecture, further enhanced by cascading trees and glowing streetlamps.",
+        imageUrl: "https://www.hdwallpapers.in/download/bridge_of_alexander_with_lights_france_paris_with_background_of_clouds_and_sky_4k_5k_hd_travel-HD.jpg"
+    },
+    {
+        title: "Amsterdam",
+        location: "Netherlands",
+        googleMapsUrl: "https://www.google.fr/maps/place/Amsterdam,+Netherlands/@52.354551,4.7391542,11z/data=!3m1!4b1!4m6!3m5!1s0x47c63fb5949a7755:0x6600fd4cb7c0af8d!8m2!3d52.3675734!4d4.9041389!16zL20vMGszcA?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "06 April 2017",
+        endDate: "30 April 2017",
+        description: "This Netherlands city takes a lax look at women beckoning business in the red-light district and coffee shops selling an unorthodox type of herb to a toking clientele, but these descriptions only scratch the surface. At some point, during an excellent Indonesian meal, a twilight canal-side rambling or a shopping excursion through the boutiques of Nine Little Streets, you'll realize as many travelers have before you that there's much more to Amsterdam than you might've thought.",
+        imageUrl: "https://wallup.net/wp-content/uploads/2017/11/22/320199-boat-building-Amsterdam.jpg"
+    }
+]
